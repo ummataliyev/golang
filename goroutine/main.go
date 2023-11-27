79e0e86764640164c6f3714c572a99725766bddf
ummataliyev <djohnummataliyev@gmail.com>
2023-11-27 21:01:57 +0500

    Goroutine & WaitGroup

--- goroutine/main.go
+++ goroutine/main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	go display("Hello!")
-	go display("Hi!")
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		display("Hello!")
+		wg.Done()
+	}()
+	go func() {
+		go display("Hi!")
+		wg.Done()
+	}()
 
-	fmt.Scanln()
+	wg.Wait()
+	fmt.Println("Completed!")
 }
 
 func display(input string) {
-	for i := 1; true; i++ {
+	for i := 1; i <= 7; i++ {
 		fmt.Println(i, input)
 		time.Sleep(1 * time.Second)
 	}
