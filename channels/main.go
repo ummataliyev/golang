11368d80da0e89651a459525d6fbe1ee25819f4d
ummataliyev <djohnummataliyev@gmail.com>
2023-11-27 23:56:00 +0500

    Channels

--- channels/main.go
+++ channels/main.go
@@ -6,20 +6,33 @@ import (
 	"time"
 )
 
+// func main() {
+
+// 	channel := make(chan string)
+// 	go func() {
+// 		channel <- "banana"
+// 		channel <- "apple"
+// 	}()
+
+// 	fmt.Println(<-channel)
+// 	fmt.Println(<-channel)
+
+// }
+
 func main() {
 	channel := make(chan int)
 	go getRandomNumber(channel)
 	for randomNumber := range channel {
-		fmt.Println("Random number is:", randomNumber)
+		fmt.Println("Radnom number is:", randomNumber)
 	}
 }
 
 func getRandomNumber(channel chan int) {
 	rand.Seed(time.Now().UnixNano())
-	for i := 1; i <= 3; i++ {
-		randomNumber := rand.Intn(100) // Generate a random number between 0 and 99
-		channel <- randomNumber
-		time.Sleep(time.Second) // Introduce a delay to demonstrate receiving in the main goroutine
+	for i := 1; i < 3; i++ {
+		number := rand.Int(1000)
+		time.Sleep(time.Second * 1)
+		channel <- number
 	}
 	close(channel)
 }
